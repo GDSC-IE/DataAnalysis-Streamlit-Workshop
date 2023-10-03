07944cb0843412e6ad162cc4dec929dec481af8b
ahubler01 <131806293+ahubler01@users.noreply.github.com>
2023-10-03 11:28:40 +0200

    Update front_flash.py
    - New flashcards display 
    - Removed "Teacher" section
    - New API component and number of flashcards

--- front_flash.py
+++ front_flash.py
@@ -21,9 +21,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 # Click on the link
 # After each modification, simply refresh the page on your web browser
 
-# Set up OpenAI API key
-st.sidebar.header("OpenAI API")
-api_key = st.sidebar.text_input("Enter your OpenAI API key")
+
+#Armand's api key: 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
+
 
 def get_file_icon(file_extension):
     # Replace this with the appropriate icons for different file types
@@ -42,7 +42,7 @@ def get_file_icon(file_extension):
 def caching(caching_docs):
     return caching_docs
 
-def context_doc(uploaded_docs, chunk_size):
+def context_doc(uploaded_docs, chunk_size, flashcard_number):
 
     def read_pdf(pdf_docs):
         temp_dir = tempfile.TemporaryDirectory()
@@ -66,12 +66,25 @@ def context_doc(uploaded_docs, chunk_size):
         print(chunks)
         return chunks
 
+    def extract_clear_text(text):
+        # Remove page numbers
+        text = re.sub(r'\n\d+\s', '\n', text)
+
+        # Remove unwanted characters and multiple spaces
+        text = re.sub(r'[^\w\s.]', '', text)
+        text = re.sub(r'\s+', ' ', text)
+
+        # Remove trailing and leading spaces
+        text = text.strip()
+
+        return text
+
 
     # Create Anki cards
-    def create_anki_cards(pdf_text, chunk_size):
+    def create_anki_cards(content, chunk_size):
 
-        divided_sections = split_chunks(pdf_text, chunk_size)
-        print(divided_sections)
+        divided_sections = split_chunks(content, chunk_size)
+
         # text = divided_sections[0]
         generated_flashcards = ' '
         for i, text in enumerate(divided_sections):
@@ -80,7 +93,7 @@ def context_doc(uploaded_docs, chunk_size):
             messages = [
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user",
-                 "content": f"Create anki flashcards with the provided text using a format: question;answer next line question;answer etc. Keep question and the corresponding answer on the same line {text}"}
+                 "content": f"Create {flashcard_number} anki flashcards with the provided text using a format: question;answer next line question;answer etc. Keep question and the corresponding answer on the same line. For each question you should mention the concept or topic related. {text}"}
             ]
 
             response = openai.ChatCompletion.create(
@@ -93,17 +106,19 @@ def context_doc(uploaded_docs, chunk_size):
             response_from_api = response['choices'][0]['message']['content']  # .strip()
             generated_flashcards += response_from_api
 
-            return generated_flashcards
+        return generated_flashcards
 
 
     pdf_text = read_pdf(uploaded_docs)
+
+    clear_pdf = extract_clear_text(pdf_text)
 
     generated_flashcards = create_anki_cards(pdf_text, chunk_size)
 
     return generated_flashcards
 
 # Initialize OpenAI API with your key
-openai.api_key = api_key
+openai.api_key = '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'
 st.session_state.process_pdf = False
 
 # Title (Must change the title)
@@ -111,6 +126,18 @@ st.markdown("<p style='font-size: 30px;'>Unleash the Power of GPT for Document I
 
 
 with st.sidebar:
+
+    # Set up OpenAI API key
+    st.sidebar.header("OpenAI API")
+    api_key = st.sidebar.text_input("Enter your OpenAI API key")
+    # Initialize OpenAI API with your key
+    openai.api_key = api_key
+
+    # Set up the number of flashcards to generate
+    st.sidebar.header("Number of Flashcards")
+    st.session_state.flashcard_number = st.sidebar.number_input(
+        "Number of flashcards to generate", min_value=1, max_value=15, value=5, step=1)
+
     st.subheader("Your documents")
     st.session_state.uploaded_docs = st.file_uploader(
         "Upload your PDFs here and click on 'Process'", accept_multiple_files=True, type=["pdf"])
@@ -155,117 +182,59 @@ with st.sidebar:
 
     #Add a new if statement
     if st.session_state.process_pdf and st.session_state.pdf_docs_cached:
-        context = context_doc(st.session_state.pdf_docs_cached, chunk_size)
-        print(context)
+        st.session_state.context = context_doc(st.session_state.pdf_docs_cached, chunk_size, st.session_state.flashcard_number)
 
 # Create a sidebar (must add icons
-type = option_menu(None, ["FLASHCARDS", "DOWNLOAD", "TEACHER"],
+type = option_menu(None, ["FLASHCARDS", "DOWNLOAD"],
                         icons=[],
                         menu_icon="cast", default_index=0, orientation="horizontal")
 
 if type == "FLASHCARDS":
-
     #Add space
     st.write("")
 
-    with st.expander('Options', expanded=True):
+    if "context" not in st.session_state:
+        st.subheader(f"Generate first your flashcards")
+    else:
+        # Function to display and edit a box of question and answer
+        def display_qna_box(q, a, index):
+            st.subheader(f"#{index + 1}")
+            question = st.text_area(
+                label="Question", value=q, key=f"question_{index}")
+            answer = st.text_area(
+                label="Answer", value=a, key=f"answer_{index}")
+            return question, answer
 
-        def cards(question, answer):
-            left_column.header(question)
-            right_column.header(answer)
+        if "edited_flashcards" not in st.session_state:
+            st.session_state.edited_flashcards = {
+                "What is 2 + 2?": "4",
+                "What is the capital of France?": "Paris",
+                "What is the largest planet in our solar system?": "Jupiter"
+            }
 
-
-        left_column, right_column = st.columns([1, 1])
-        st.session_state.question = "Here is my initial question"
-        st.session_state.answer = "Here is my initial answer"
-        # Initialize a session state variable
-        if 'click_count' not in st.session_state:
-            st.session_state.click_count = 0
-            cards(st.session_state.question, st.session_state.answer)
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-
-        # List of labels to toggle between
-        labels = ["Edit", "Validate"]
-
-        # Display a button that toggles between labels
-        button_label = labels[st.session_state.click_count % 2]
-        st.session_state.button_clicked = col3.button(button_label)
-
-        # Update the session state on button click
-        if st.session_state.button_clicked or not st.session_state.button_clicked:
-            st.session_state.click_count += 1
-
-        st.markdown(button_label)
-        st.markdown(st.session_state.click_count)
-
-        if button_label =="Validate":
-            st.session_state.question = left_column.text_area("Edit your flashcard", value=st.session_state.question)
-            st.session_state.answer = right_column.text_area("Edit your flashcard", value=st.session_state.answer)
-            st.markdown(f"**Question:** {st.session_state.question}\n\n**Answer:** {st.session_state.answer}")
-            question = left_column.text_area("Edit your flashcard", value="My text")
-            # cards(st.session_state.question, st.session_state.answer)
-        elif button_label =="Edit" and st.session_state.click_count!=1:
-            cards(st.session_state.question, st.session_state.answer)
-
-
-    if st.session_state.process_pdf:
-        st.subheader("Your flashcards")
+        if 'edited_flashcards' in st.session_state:
+            # Display and edit all boxes
+            for i, (question, answer) in enumerate(st.session_state.edited_flashcards.copy().items()):
+                edited_q, edited_a = display_qna_box(question, answer, i)
+                if edited_q != question or edited_a != answer:
+                    del st.session_state.edited_flashcards[question]
+                    st.session_state.edited_flashcards[edited_q] = edited_a
 
 
 if type == "DOWNLOAD":
-    st.button("Download your flashcards")
-    # Call the function to write the file
-    # Armand: I have it, but I just need to add it
 
-    #This is the code:
-    # # # Save the cards to a text file
-    # with open("flashcards.txt", "w") as f:
-    #     f.write(generated_flashcards)
+    if "string_edited_flashcards" not in st.session_state:
+        st.session_state.string_edited_flashcards = ""
 
-if type =="TEACHER":
+    # Transform the dictionary into a list of key-value pairs and iterate through it
+    for key, value in st.session_state.edited_flashcards.items():
+        st.session_state.string_edited_flashcards += ', '.join([f"{key}: {value}"])
+        st.session_state.string_edited_flashcards += "\n"
 
-    # Clear the chat conversation when the button is clicked
-    if st.button("Clear conversation"):
-        st.session_state['messages'] = []
+    if ':' in st.session_state.string_edited_flashcards:
+        st.session_state.string_edited_flashcards = st.session_state.string_edited_flashcards.replace(':', ';')
 
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    #Initialize for the moment
+    st.download_button('Download Flashcards', st.session_state.string_edited_flashcards, 'my_flashcards.txt')
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Chat_input
-    prompt = st.chat_input("What can I do for you?")
-
-    # Process user input and respond using the selected language model
-    if prompt:
-        st.session_state.prompt_state = True
-        st.session_state.prompt = prompt
-    else:
-        st.session_state.prompt_state = False
-
-    if st.session_state.prompt_state:
-        with st.chat_message("user"):
-            st.markdown(st.session_state.prompt)
-
-        if st.session_state.process:
-
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            print("in process")
-
-            # Generate a response
-            #Call the function which returns a list with the answer and the question
-
-            with st.chat_message("assistant"):
-                st.markdown(response[0])
-                st.session_state.messages.append({"role": "assistant", "content": response[0]})
-
-        else:
-            with st.chat_message("assistant"):
-                st.markdown("Please upload your file")
 
